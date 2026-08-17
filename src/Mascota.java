0168f3399ff900b607e90d796121546dda5a8a5d
ALEX OLIVARES <alexolivares220@gmail.com>
2026-08-17 15:36:04 -0300

    Ejercicio 8 Registro Mascota.java

--- src/Mascota.java
+++ src/Mascota.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
-
 public class Mascota {
+
+    String nombre;
+    String especie;
+    int edad;
+
+
+    public Mascota(String nombre, String especie, int edad) {
+
+        this.edad = edad;
+        this.nombre = nombre;
+        this.especie = especie;
+
+    }
+
+    public void Incremento(){
+
+        edad++;
+
+    }
+
+    public void mostrar(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Especie: "+especie);
+        System.out.println("Edad: "+edad+" años");
+    }
+
+    public static void main(String[] args) {
+
+        Mascota mascota = new Mascota("Simón", "Pitbull", 4);
+        mascota.mostrar();
+        mascota.Incremento();
+
+        System.out.println("================= 1 año después =================");
+
+        mascota.mostrar();
+        mascota.Incremento();
+
+        System.out.println("================= 2 año después =================");
+        mascota.mostrar();
+        mascota.Incremento();
+
+        System.out.println("================= 3 año después =================");
+        mascota.mostrar();
+    }
 }
